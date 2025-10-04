cc94dbc03b090d4e73b155983b52c3ce9bf5f898
Maja-1 <maja.lukacevic23@gmail.com>
2025-10-04 13:58:07 +0200

    Update 03_date_range_exploration.sql

--- scripts/03_date_range_exploration.sql
+++ scripts/03_date_range_exploration.sql
@@ -18,6 +18,10 @@ SELECT
     DATEDIFF(MONTH, MIN(order_date), MAX(order_date)) AS order_range_months
 FROM gold.fact_sales;
 
+/*
+The data confirms a 37-month period of sales activity, starting in late 2010 and ending in early 2014.
+*/
+
 -- Find the youngest and oldest customer based on birthdate
 SELECT
     MIN(birthdate) AS oldest_birthdate,
@@ -25,3 +29,7 @@ SELECT
     MAX(birthdate) AS youngest_birthdate,
     DATEDIFF(YEAR, MAX(birthdate), GETDATE()) AS youngest_age
 FROM gold.dim_customers;
+
+/*
+From the data we can see that oldest customer is 109 years old and youngest is 39 years old
+*/
