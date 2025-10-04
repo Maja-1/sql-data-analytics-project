e353224d6e4b7645d4c18e4c136989e77afe8145
Maja-1 <maja.lukacevic23@gmail.com>
2025-10-04 14:30:23 +0200

    Update 04_measures_exploration.sql

--- scripts/04_measures_exploration.sql
+++ scripts/04_measures_exploration.sql
@@ -45,3 +45,14 @@ UNION ALL
 SELECT 'Total Products', COUNT(DISTINCT product_name) FROM gold.dim_products
 UNION ALL
 SELECT 'Total Customers', COUNT(customer_key) FROM gold.dim_customers;
+
+
+/*
+Based on these metrics, the business generates significant Total Sales of over 29 million, distributed across 27,659 orders.
+A key analytical highlight is the exceptional data integrity, as 100% of the customers (18,484) registered in the system 
+have also placed an order. 
+In our further analysis, we will calculate the Estimated Average Order Value (AOV) by dividing Total Sales by Total Orders, 
+and the Average Quantity per Order by dividing Total Quantity by Total Orders. 
+These calculations will clearly demonstrate that the current strategy is successfully focused on selling high-value products. 
+
+*/
