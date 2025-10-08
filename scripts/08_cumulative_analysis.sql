0a936c9e9b68ef3eaab89fc00314031a04fd9110
Maja-1 <maja.lukacevic23@gmail.com>
2025-10-08 16:29:54 +0200

    Update 08_cumulative_analysis.sql

--- scripts/08_cumulative_analysis.sql
+++ scripts/08_cumulative_analysis.sql
@@ -29,3 +29,15 @@ FROM
     WHERE order_date IS NOT NULL
     GROUP BY DATETRUNC(year, order_date)
 ) t
+
+/*
+The cumulative (running total) sales show strong, consistent growth, increasing 
+from $43,419 in December 2010 to over $29 million by January 2014, indicating a 
+very successful overall business expansion. However, this growth has occurred 
+alongside a continuous and significant decline in the average sales price (the 
+moving average price), which dropped from $$$3,101 to $1,745.
+
+This suggests the company's revenue growth is primarily driven by selling a much 
+higher volume of units, potentially through lower pricing strategies, discounted 
+sales, or a shift in the sales mix towards less expensive products.
+*/
