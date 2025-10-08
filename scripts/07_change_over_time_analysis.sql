c9b3a6ebbf56a2e153f723c6a979b736992bc762
Maja-1 <maja.lukacevic23@gmail.com>
2025-10-08 12:58:09 +0200

    Update 07_change_over_time_analysis.sql

--- scripts/07_change_over_time_analysis.sql
+++ scripts/07_change_over_time_analysis.sql
@@ -34,6 +34,12 @@ collapsed in 2014, wiping out all the recent growth.
 gold addditional We must immediately investigate the cause of the 2014 failure 
 and understand what exactly was sold during the 2013 boom.
 
+Monthly report
+This data shows a clear growth trend in sales throughout the year, peaking in the last two months. 
+December was the strongest month, with the highest total sales ($3,211,396) and the largest number of customers (2,656). 
+The increase in sales seems to be driven by more customers buying more items, particularly during the holiday shopping 
+season in the final quarter. This suggests that marketing efforts are most effective late in the year, and the company 
+should look for ways to boost sales in the earlier, slower months.
 */
 
 -- DATETRUNC()
@@ -57,4 +63,5 @@ FROM gold.fact_sales
 WHERE order_date IS NOT NULL
 GROUP BY FORMAT(order_date, 'yyyy-MMM')
 ORDER BY FORMAT(order_date, 'yyyy-MMM');
+
 
